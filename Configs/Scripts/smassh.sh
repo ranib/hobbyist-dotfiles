38a1f63fea80a6c9fae89dfbfb987cccb8d99698
BlackSparkz⁭⁭⁭ <1g39oe1r@duck.com>
2026-05-22 12:04:33 +0530

    Aichat added

--- Configs/Scripts/smassh.sh
+++ Configs/Scripts/smassh.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 FOOT_CONF="$HOME/.config/foot/foot_for_smassh.ini"
-foot --fullscreen --config $FOOT_CONF --app-id smassh -e smassh
+
+if command -v smassh &>/dev/null; then
+  foot --fullscreen --config $FOOT_CONF --app-id smassh -e smassh
+else
+  notify-send "Smassh not found" "Please install smassh"
+fi
