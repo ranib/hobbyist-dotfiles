8d9e4d597d90f5e9716fe97765f26befe126d062
BlackSparkz⁭⁭⁭ <1g39oe1r@duck.com>
2026-05-27 10:11:30 +0530

    installation script updated

--- Configs/hypr/hyprland_modules/Look_and_feel.lua
+++ Configs/hypr/hyprland_modules/Look_and_feel.lua
@@ -9,7 +9,6 @@ hl.config({
         border_size = 2,
 
         col = {
-            -- active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
             active_border   = "rgba(ffffffff)",
             inactive_border = "rgba(00000000)",
         },
@@ -71,15 +70,15 @@ hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}
 hl.curve("quick",          { type = "bezier", points = { {0.1, 0},     {0.0, 1}     } })
 
 hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
-hl.curve("hobbyist",       { type = "spring", mass = 1, stiffness = 40, dampening = 5 } )
+hl.curve("hobbyist",       { type = "spring", mass = 1, stiffness = 40, dampening = 6 } )
 hl.curve("cat",            { type = "spring", mass = 1, stiffness = 30, dampening = 6 } )
 
 hl.animation({ leaf = "global",        enabled = true,  speed = 8,    bezier = "default" })
-hl.animation({ leaf = "border",        enabled = true,  speed = 2,    bezier = "almostLinear" })
-hl.animation({ leaf = "windows",       enabled = true,  speed = 8,    spring = "cat",        style = "slide" })
-hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 8,    spring = "cat",        style = "slide" })
-hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 8,    spring = "cat",        style = "slide bottom" })
-hl.animation({ leaf = "windowsMove",   enabled = true,  speed = 6,    spring = "hobbyist" })
+hl.animation({ leaf = "border",        enabled = true,  speed = 4,    bezier = "almostLinear" })
+hl.animation({ leaf = "windows",       enabled = true,  speed = 10,    spring = "cat",        style = "slide" })
+hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 10,    spring = "cat",        style = "slide" })
+hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 10,    spring = "cat",        style = "slide bottom" })
+hl.animation({ leaf = "windowsMove",   enabled = true,  speed = 10,    spring = "hobbyist" })
 hl.animation({ leaf = "fadeIn",        enabled = true,  speed = 1.73, bezier = "almostLinear" })
 hl.animation({ leaf = "fadeOut",       enabled = true,  speed = 1.46, bezier = "almostLinear" })
 hl.animation({ leaf = "fade",          enabled = true,  speed = 3.03, bezier = "quick" })
@@ -88,10 +87,10 @@ hl.animation({ leaf = "layersIn",      enabled = true,  speed = 4,    bezier = "
 hl.animation({ leaf = "layersOut",     enabled = true,  speed = 3,    bezier = "linear",       style = "slide bottom" })
 hl.animation({ leaf = "fadeLayersIn",  enabled = true,  speed = 1.79, bezier = "almostLinear" })
 hl.animation({ leaf = "fadeLayersOut", enabled = true,  speed = 1.39, bezier = "almostLinear" })
-hl.animation({ leaf = "workspaces",    enabled = true,  speed = 8,    spring = "hobbyist",     style = "slidevert" })
-hl.animation({ leaf = "workspacesIn",  enabled = true,  speed = 8,    spring = "hobbyist",     style = "slidevert" })
-hl.animation({ leaf = "workspacesOut", enabled = true,  speed = 8,    spring = "hobbyist",     style = "slidevert" })
-hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 6,    bezier = "quick" })
+hl.animation({ leaf = "workspaces",    enabled = true,  speed = 12,    spring = "hobbyist",     style = "slidevert" })
+hl.animation({ leaf = "workspacesIn",  enabled = true,  speed = 12,    spring = "hobbyist",     style = "slidevert" })
+hl.animation({ leaf = "workspacesOut", enabled = true,  speed = 12,    spring = "hobbyist",     style = "slidevert" })
+hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 8,    bezier = "quick" })
 
 hl.config({
   dwindle = {
